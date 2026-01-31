eadcf5b9d81783e8714cac3d229c875111917e50
Avantika Pandey <er.avantikapandey@gmail.com>
2026-01-31 16:43:12 +0530

    Updated config/urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("analytics.urls")),
+    path("healthz", healthz),
 ]
 
 if settings.DEBUG:
