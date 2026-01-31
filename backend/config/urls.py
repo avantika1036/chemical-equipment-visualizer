3abec2c42bdd1f177e128419d3c2f6e6802346e9
Avantika Pandey <er.avantikapandey@gmail.com>
2026-01-31 16:41:27 +0530

    Updated config/urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,21 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import JsonResponse
-
-def healthz(request):
-    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
     path("api/", include("analytics.urls")),
-    path("api/auth/", include("authentication.urls")),
-    path("healthz", healthz),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
